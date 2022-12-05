b366a9ae2beff596c1f2a6bc06ce27e3e189b39c
Tom <t.chapman3@newcastle.ac.uk>
2022-12-05 11:24:18 +0000

    Add docstring to parser

--- BooleanExpressionParser/Parser.cs
+++ BooleanExpressionParser/Parser.cs
@@ -6,6 +6,11 @@ class Parser
     {
     }
 
+    /// <summary>
+    /// Converts a list of tokens in infix notation to a list of tokens in prefix notation.
+    /// </summary>
+    /// <param name="tokens">The tokens to parse.</param>
+    /// <returns>The root node of the expression tree.</returns>
     public IEnumerable<Token> ParseTokens(IEnumerable<Token> tokens)
     {
         var output = new Queue<Token>();
