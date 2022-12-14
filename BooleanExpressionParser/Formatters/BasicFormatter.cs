3e26d3363ce92dddc05bba1c958a5585543f6e3c
Tom Chapman <tomchapman128@gmail.com>
2022-12-14 16:02:14 +0000

    Add new BasicFormatter

---
+++ BooleanExpressionParser/Formatters/BasicFormatter.cs
@@ -0,0 +1,49 @@
+using System.Text;
+
+namespace BooleanExpressionParser.Formatter;
+
+
+class BasicFormatter : IFormatter
+{
+    public string FormatTokens(IEnumerable<Token> tokens)
+    {
+        var sb = new StringBuilder();
+
+        foreach (var token in tokens)
+        {
+            string s = token.ToString()!;
+            if (token is not VariableToken && s.Length > 1) s = $"[{s}]";
+            sb.Append(s);
+        }
+
+        return sb.ToString();
+    }
+
+    public string FormatTruthTable(Ast ast, List<bool[]> table, string label = "Result")
+    {
+        // Return a string representation of the truth table, that is simplistic and machine readable
+        // Format: <var1>,<var2>,...<label>;<row1,0>,<row1,1>,...<row1,result>;...
+
+        var sb = new StringBuilder();
+
+        // Header
+        sb.AppendJoin(',', ast.Variables);
+        sb.Append(',');
+        sb.Append(label);
+        sb.Append(';');
+
+        // Rows
+        for (int i = 0; i < table.Count; i++)
+        {
+            sb.AppendJoin(',', table[i].Select(b => b ? "1" : "0"));
+            sb.Append(';');
+        }
+
+        return sb.ToString();
+    }
+
+    public string JoinTruthTables(params string[] tables)
+    {
+        throw new NotImplementedException();
+    }
+}
